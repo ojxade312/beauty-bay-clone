6d64161764e3e20694141de409050e1280ad44cd
Ayomide Ojelade <ojeladea@outlook.com>
2022-04-12 22:54:00 +0100

    setting up typescript

---
+++ beauty-bay/src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
+      <div>"Hi"</div>
+    </div>
+  );
+}
+
+export default App;
