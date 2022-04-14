b420604ba1d6921b495ee993c736602942faf787
Ayomide Ojelade <ojeladea@outlook.com>
2022-04-14 16:50:34 +0100

    react router setup

---
+++ beauty-bay/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    // <div className="App">
+    //   <Routes>{ <Route path="/" element={<Home />} /> }</Routes>
+    //   <div>"Hi"</div>
+    // </div>
+    <div>"Hi"</div>
+  );
+}
+
+export default App;
